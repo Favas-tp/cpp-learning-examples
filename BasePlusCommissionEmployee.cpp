57e377e97a6bc7b36a2f0b8a8a8604aa6af60d63
Favas-tp <135324278+Favas-tp@users.noreply.github.com>
2023-06-09 07:08:26 +0530

    Update BasePlusCommissionEmployee.cpp

--- BasePlusCommissionEmployee.cpp
+++ BasePlusCommissionEmployee.cpp
@@ -9,3 +9,25 @@ BasePlusCommissionEmployee::BasePlusCommissionEmployee( const string &first, con
 {
     setBaseSalary( salary );
 }   
+
+void BasePlusCommissionEmployee::setBaseSalary( double salary )
+{
+     baseSalary = ( salary < 0.0 ) ?  0.0 : salary;
+}
+
+double BasePlusCommissionEmployee::getBaseSalary() const
+{
+    return baseSalary;
+}
+
+double BasePlusCommissionEmployee::earnings() const
+{
+    return getBaseSalary() + CommissionEmployee::earnings();
+}
+
+void BasePlusCommissionEmployee::print() const
+{
+    cout << "base-salaried ";
+    CommissionEmployee::print(); //invokes the function
+    cout << "\nbase salary: " << getBaseSalary(); 
+}
